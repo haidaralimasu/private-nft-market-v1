a91659e8dd7d43783056e40f4f8a9083742a052b
Haidarali Masu <haidaralimasu123@gmail.com>
2022-05-14 11:45:16 +0530

    inititial commit

---
+++ client/src/contracts/index.js
@@ -0,0 +1,2 @@
+export const proxyAddress = "0xa24B2375A6A07835C8fd412018ecC923Acd5C1dd";
+export const markteAddress = "0x0e309A96c44C0901bFF60D057ee1B44a7d8C75c8";
---
+++ client/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ChainId, DAppProvider } from "@usedapp/core";
+
+const config = {
+  readOnlyChainId: ChainId.Mumbai,
+  readOnlyUrls: {
+    [ChainId.Mumbai]:
+      "https://polygon-mumbai.g.alchemy.com/v2/KgzzOtuzu6LdaTaCM-ct1ya7uh45P6Lt",
+  },
+  supportedChains: [ChainId.Mumbai],
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <DAppProvider config={config}>
+      <App />
+    </DAppProvider>
+  </React.StrictMode>
+);
